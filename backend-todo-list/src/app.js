83ca480fb942836be571c037887e5099fc585f72
unknown <fernandosic11@gamil.com>
2025-12-14 01:20:03 -0600

    correccion de cors

--- backend-todo-list/src/app.js
+++ backend-todo-list/src/app.js
@@ -13,7 +13,8 @@ connectDB();
 
 // Cors
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 
+        'https://to-do-list-eight-kappa-30.vercel.app'],
     credentials: true
 }));
 
